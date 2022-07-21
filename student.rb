d771ee96125b21d582b9dec1da3da29b8a13a28b
mar <marilirulita@gmail.com>
2022-07-21 03:40:54 -0700

    Add classroom class and create relation has many- belongs to relation with student

--- student.rb
+++ student.rb
@@ -1,16 +1,27 @@
 require './person'
+require './classroom'
 
 # child class inherits from person
 class Student < Person
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+  attr_reader :classroom
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
-    @classroom = classroom
   end
 
   def play_hooky
     "¯\(ツ)/¯"
   end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
+  end
 end
 
-student1 = Student.new(25, 'math', 'monic', parent_permission: false)
+student1 = Student.new(25, 'monic', parent_permission: false)
 p student1
+math = Classroom.new("math")
+p math
+math.add_student(student1)
+p math.students
