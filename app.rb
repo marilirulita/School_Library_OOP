0fcf1926b6d1fe5e9519677c6f8c318ace97cadd
mar <marilirulita@gmail.com>
2022-07-23 04:11:32 -0700

    create main file and options string

---
+++ app.rb
@@ -0,0 +1,26 @@
+def list_all_books
+
+end
+
+def list_all_people
+
+end
+
+def create_a_person
+  print "Do you want to create a student (1) or a teacher (2)? [input the number]: "
+  selected = gets.chomp
+  selected = selected.to_i
+  p selected
+end
+
+def create_a_book
+
+end
+
+def create_a_rental
+
+end
+
+def list_all_rental_by_id
+
+end
---
+++ main.rb
@@ -0,0 +1,19 @@
+require './app'
+
+def main
+# Present the user with a list of options to perform.
+puts "Welcome to School Library App!"
+puts ""
+puts "Please choose an option by enterin a number:"
+options = ['List all books', 'List all people', 'Create a person', 'Create a book', 'Create a rental', 'List all rentals for a given person id', 'Exit']
+options.each_with_index{ |v, k| puts "#{k+1} - #{v}" }
+
+# Lets users choose an option.
+selected = gets.chomp
+selected = selected.to_i
+# If needed, ask for parameters for the option.
+create_a_person() if selected == 3
+# Have a way to quit the app.
+end
+
+main()
