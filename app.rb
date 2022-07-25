79ba7669c010560acb08ddd5a1180acbc408857f
mar <marilirulita@gmail.com>
2022-07-25 16:05:03 -0700

    Fix length linters in app rb

--- app.rb
+++ app.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-# rubocop:disable Metrics/CyclomaticComplexity
-# rubocop:disable Metrics/PerceivedComplexity
 require './student'
 require './teacher'
 require './book'
@@ -28,27 +26,32 @@ end
 def create_a_person
   print 'Do you want to create a student (1) or a teacher (2)? [input the number]: '
   selected = gets.chomp
-  selected = selected.to_i
   print 'Age: '
   age = gets.chomp
-  age = age.to_i
   print 'Name: '
   name = gets.chomp
 
-  print 'Has parent permission? [Y/N]: ' if selected == 1
-  permission = gets.chomp if selected == 1
+  create_student(age, name) if selected.to_i == 1
+  create_teacher(age, name) if selected.to_i == 2
+
+  puts 'Person created succesfully!'
+  main
+end
+
+def create_student(age, name)
+  print 'Has parent permission? [Y/N]: '
+  permission = gets.chomp
   permission = true if %w[y Y].include?(permission)
   permission = false if %w[n N].include?(permission)
 
-  print 'Specialization: ' if selected == 2
-  speciality = gets.chomp if selected == 2
+  @people.push(Student.new(age.to_i, name, parent_permission: permission))
+end
 
-  @people.push(Student.new(age, name, parent_permission: permission)) if selected == 1
-  @people.push(Teacher.new(age, speciality, name)) if selected == 2
+def create_teacher(age, name)
+  print 'Specialization: '
+  speciality = gets.chomp
 
-  puts 'Person created succesfully!'
-
-  main
+  @people.push(Teacher.new(age.to_i, speciality, name))
 end
 
 def create_a_book
@@ -65,40 +68,43 @@ def create_a_book
 end
 
 def create_a_rental
-  puts 'Selecte a book from the following list by number'
-  @books.each_with_index do |v, i|
-    puts "#{i}) Title: \"#{v.tittle}\", Author: #{v.author}"
-  end
-
+  select_book
   selected_book = gets.chomp
-  selected_book = selected_book.to_i
 
-  puts 'Select a person from the followin list by number (not id)'
-  @people.each_with_index do |v, i|
-    puts "#{i}) [#{v.class.name}] Name: #{v.name}, ID: #{v.id}, Age: #{v.age}"
-  end
-
+  select_person
   selected_person = gets.chomp
-  selected_person = selected_person.to_i
 
   print 'Date: '
   date = gets.chomp
 
-  Rental.new(date, @people[selected_person], @books[selected_book])
+  Rental.new(date, @people[selected_person.to_i], @books[selected_book.to_i])
   puts 'Rental created succesfully!'
 
   main
 end
 
+def select_book
+  puts 'Selecte a book from the following list by number'
+  @books.each_with_index do |v, i|
+    puts "#{i}) Title: \"#{v.tittle}\", Author: #{v.author}"
+  end
+end
+
+def select_person
+  puts 'Select a person from the followin list by number (not id)'
+  @people.each_with_index do |v, i|
+    puts "#{i}) [#{v.class.name}] Name: #{v.name}, ID: #{v.id}, Age: #{v.age}"
+  end
+end
+
 def list_all_rental_by_id
   print 'ID of person: '
   id = gets.chomp
-  id = id.to_i
 
   puts 'Rentals: '
 
   @people.map do |v|
-    next unless v.id == id
+    next unless v.id == id.to_i
 
     v.rentals.each do |n|
       puts "Date: #{n.date}, Book \"#{n.book.tittle}\" by #{n.book.author}"
@@ -111,5 +117,3 @@ end
 def exit
   puts 'Thank you for using this app!'
 end
-# rubocop:enable Metrics/CyclomaticComplexity
-# rubocop:enable Metrics/PerceivedComplexity
--- main.rb
+++ main.rb
@@ -2,7 +2,20 @@
 
 require './app'
 
-def main(option)
+def main
+  options = [
+    { id: 1, text: 'List all books', method: 'list_all_books' },
+    { id: 2, text: 'List all people', method: 'list_all_people' },
+    { id: 3, text: 'Create a person', method: 'create_a_person' },
+    { id: 4, text: 'Create a book', method: 'create_a_book' },
+    { id: 5, text: 'Create a rental', method: 'create_a_rental' },
+    { id: 6, text: 'List all rentals for a given person id', method: 'list_all_rental_by_id' },
+    { id: 7, text: 'Exit', method: 'exit' }
+  ]
+  option_cycle(options)
+end
+
+def option_cycle(option)
   puts ''
   puts 'Please choose an option by enterin a number:'
 
@@ -13,18 +26,6 @@ def main(option)
   option.each { |v| send(v[:method]) if v[:id] == selected.to_i }
 end
 
-def options
-  [
-    { id: 1, text: 'List all books', method: 'list_all_books' },
-    { id: 2, text: 'List all people', method: 'list_all_people' },
-    { id: 3, text: 'Create a person', method: 'create_a_person' },
-    { id: 4, text: 'Create a book', method: 'create_a_book' },
-    { id: 5, text: 'Create a rental', method: 'create_a_rental' },
-    { id: 6, text: 'List all rentals for a given person id', method: 'list_all_rental_by_id' },
-    { id: 7, text: 'Exit', method: 'exit' }
-  ]
-end
-
 puts 'Welcome to School Library App!'
 
-main(options)
+main
