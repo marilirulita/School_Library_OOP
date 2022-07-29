71cac6ba60a47c543e55d9d1a033ba271767a78a
oshanedesign <oshane.design@gmail.com>
2022-07-29 09:47:28 -0500

    tests for classroom decorator rental

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -1,1 +1,24 @@
 require 'spec_helper'
+
+describe Classroom do
+    before(:all) do 
+      @classroom = Classroom.new "Fisic"
+      @student = Student.new 20, "Jhon"
+    end 
+  
+    it "takes one parameter and returns a Classroom object" do
+      expect(@classroom).to be_instance_of Classroom
+    end
+  
+    it "returns label value from Classroom object" do
+      expect(@classroom.label).to eq("Fisic")
+    end
+  
+    it "add_student method takes one Student object as a parameter" do
+      expect(@classroom.add_student(@student)).not_to be_nil
+    end
+    it "students return an array of Student objects" do
+      expect(@classroom.students).not_to be_nil
+    end
+  end
+  
