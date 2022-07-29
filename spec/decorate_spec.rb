9b4209e6f3b8bae2a14549d038ddadfd0e338e10
mar <marilirulita@gmail.com>
2022-07-29 08:03:16 -0700

    fix linters in test files

--- spec/decorate_spec.rb
+++ spec/decorate_spec.rb
@@ -1,35 +1,34 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe Decorator do
-    before(:all) do
-      @person = Person.new 17, "dany", parent_permission: false
-      @person2 = Person.new 25, "george maximilian"
-      @decorator = Decorator.new(@person)
-      @capitalize_decorator = CapitalizeDecorator.new(@person)
-      @trimmer_decorator = TrimmerDecorator.new(@person)
-      @trimmer_decorator2 = TrimmerDecorator.new(@person2)
-    end 
-  
-    it "takes one person object as a parameter and returns a Decorator object" do
-      expect(@decorator).to be_instance_of Decorator
-    end
-  
-    it "correct_name method returns name value from Person object" do
-      expect(@decorator.correct_name).to eq("dany")
-    end
-  
-    it "correct_name method returns name value capitalized from Person object" do
-      expect(@capitalize_decorator.correct_name).to eq("Dany")
-    end
-  
-    it "correct_name method returns name lenght from Person object" do
-      expect(@trimmer_decorator.correct_name.length).to be <= 10
-    end
-  
-    it "correct_name method returns name length value to be less than 10 from Person object" do
-        expect(@trimmer_decorator2.correct_name.length).to be <= 10
-    end
-  
+  before(:all) do
+    @person = Person.new 17, 'dany', parent_permission: false
+    @person2 = Person.new 25, 'george maximilian'
+    @decorator = Decorator.new(@person)
+    @capitalize_decorator = CapitalizeDecorator.new(@person)
+    @trimmer_decorator = TrimmerDecorator.new(@person)
+    @trimmer_decorator2 = TrimmerDecorator.new(@person2)
   end
-  
-  
+
+  it 'takes one person object as a parameter and returns a Decorator object' do
+    expect(@decorator).to be_instance_of Decorator
+  end
+
+  it 'correct_name method returns name value from Person object' do
+    expect(@decorator.correct_name).to eq('dany')
+  end
+
+  it 'correct_name method returns name value capitalized from Person object' do
+    expect(@capitalize_decorator.correct_name).to eq('Dany')
+  end
+
+  it 'correct_name method returns name lenght from Person object' do
+    expect(@trimmer_decorator.correct_name.length).to be <= 10
+  end
+
+  it 'correct_name method returns name length value to be less than 10 from Person object' do
+    expect(@trimmer_decorator2.correct_name.length).to be <= 10
+  end
+end
--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -1,23 +1,24 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe Rental do
-    before(:all) do 
-      @book = Book.new "Year Book", "Year Author"
-      @person = Person.new 16, "Marc", parent_permission: false
-      @date = '15-03-2020'
-      @rental = Rental.new @date, @person, @book
-    end 
-  
-    it "takes three parameters and returns a Rental object" do
-      expect(@rental).to be_instance_of Rental
-    end
-  
-    it "returns book object from Rental object" do
-      expect(@rental.book).to eq(@book)
-    end
-  
-    it "returns person object from Rental object" do
-      expect(@rental.person).to eq(@person)
-    end
+  before(:all) do
+    @book = Book.new 'Year Book', 'Year Author'
+    @person = Person.new 16, 'Marc', parent_permission: false
+    @date = '15-03-2020'
+    @rental = Rental.new @date, @person, @book
   end
-  
+
+  it 'takes three parameters and returns a Rental object' do
+    expect(@rental).to be_instance_of Rental
+  end
+
+  it 'returns book object from Rental object' do
+    expect(@rental.book).to eq(@book)
+  end
+
+  it 'returns person object from Rental object' do
+    expect(@rental.person).to eq(@person)
+  end
+end
