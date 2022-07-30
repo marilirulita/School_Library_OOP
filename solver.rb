9fbcd1c53807df7876283a2e083bbbae98a00575
mar <marilirulita@gmail.com>
2022-07-30 15:58:56 -0700

    Add answer for negative numbers in factorial method

--- solver.rb
+++ solver.rb
@@ -1,11 +1,14 @@
 class Solver
   def factorial(n)
-    return 1 if n == 0
-    answer = n
+    if n >= 0
+      return 1 if n == 0
+      answer = n
       until n == 1
         n = n -1
         answer = answer * n
       end
-    return answer
+      return answer
+    end
+    return "Not a valid number"
   end
 end
