d2c0d70830308baab462d28f4ed9b8ead13ec660
mar <marilirulita@gmail.com>
2022-07-30 15:53:41 -0700

    Add second test and system fails

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -10,5 +10,8 @@ describe Solver do
       it 'expects factorial of 5 to be 120' do
         expect(@object.factorial(5)).to eq(120)
       end
+      it 'expects factorial of 0 to be 1' do
+        expect(@object.factorial(0)).to eq(1)
+      end
   end
 end
